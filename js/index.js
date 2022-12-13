0a64547271acbb60f6aad99dd208b1d012964988
ryan605 <langatryan26@gmail.com>
2022-12-13 21:17:17 +0300

    done

--- js/index.js
+++ js/index.js
@@ -1,56 +1,94 @@
-
-function fetchData(input)
-{
-    fetch(`https://api.github.com/users/${input}`, 
-    {
-        method: 'GET',
-        headers: 
-        {
-            'Accept': 'application/vnd.github.v3+json',
-        },
-    })
-    
-    .then(res=>
-        {
-            return res.json()
-        })
-    .then(data=>
-        {
-            const users =  document.getElementById('user-list');
-            const list = document.createElement('li');
-            list.innerHTML = `USERNAME: ${data.login} <br>
-                              REPOSITORY: ${data.avatar_url}
-                             `;
-            users.appendChild(list)
-            console.log(data.login)
-            console.log(data)
-        }).catch(err=> console.log('err'))
+function appendElement(element, id = "animal-item-body") {
+    const rootElement = document.getElementById(id);
+    rootElement.append(element);
 }
 
-function formSubmit()
-{
-    const form = document.getElementById('github-form')
-    form .addEventListener('submit', (e)=>
-    {
-        e.preventDefault();
-        const input = e.target.search.value
-       fetchData(input)
+function listElements(login, id){
+    const root = document.getElementById("user-list")
+
+    let loginName = document.createElement("li")
+    loginName.innerHTML = login
+    loginName.style.cursor = "pointer"
+    loginName.addEventListener('click',(e)=>{
         
+        fetchRepo(e,id)
+        let display = document.getElementById("repos-list")
+        display.innerHTML = ""
     })
+
+
+    root.append(loginName)
+
+    return root;
 }
- function renderSearch(dat)
- {
-     for(let i = 0; i <= i.length; i++)
-     {
-         const users =  document.getElementById('user-list');
-         const list = document.createElement('li');
-         list.innerHTML = `${dat[i].login}`;
-         users.appendChild(list);
-     }   
+function createRepos(name){
+    let rootRepos = document.getElementById("repos-list");
+    
+    let repsName = document.createElement('li');
+    repsName.innerHTML = name;
+
+    rootRepos.append(repsName)
+
+    return rootRepos;
 }
-document.addEventListener('DOMContentLoaded', ()=>
-{
-    fetchData()
-    formSubmit()
-   renderSearch()
-})
+
+function fetchRepo(e,id){
+    let repoName = (e.target.innerHTML)
+    console.log(repoName)
+
+    fetch(`https://api.github.com/users/${repoName}/repos`)
+    .then(res=> res.json())
+    .then((data) => {
+        console.log(data)
+
+        for(let i=0; i<data.length; i++ ){
+            let repoNames = data[i]
+            const repositoryNames = createRepos(repoNames.name)
+            appendElement(repositoryNames, "github-container")
+
+        }
+
+        })}
+
+
+function handleSubmit(){
+    let endpoint = document.querySelector('#search').value
+    
+
+    fetch(`https://api.github.com/search/users?q=${endpoint}`,{
+        //method:'GET',
+        //headers: {'Content-type': 'application/vnd.github.v3+json'}
+    })
+    .then(res=> res.json())
+    .then((data) =>{
+        console.log(data)
+        let content = data.items
+        for(let i=0; i<content.length; i++){
+            let listNames = content[i]
+            let listDb = listElements(listNames.login, listNames.id)
+            appendElement(listDb, "github-container")
+        }
+
+        console.log(data)
+
+    })
+
+        
+
+    console.log(`hello ${endpoint}`)
+}
+
+//function getReposNames(){
+
+//}
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+    document.querySelector("form").addEventListener('submit',(e)=>{
+      e.preventDefault()
+      handleSubmit()
+  
+    })
+  
+  
+});
